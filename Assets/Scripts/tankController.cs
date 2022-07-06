fc36b67a036f3afded3d8ab1ddaf48498d5a51d6
Akhil Sharma <akhilsharma.off@gmail.com>
2022-07-07 03:02:45 +0530

    Implemented the MVC Functionality in the project

---
+++ Assets/Scripts/tankController.cs
@@ -0,0 +1,19 @@
+﻿
+using UnityEngine;
+
+public class tankController 
+{
+    private tankModel TankModel;
+    private tankView TankView;
+
+    public tankController(tankModel _tankModel, tankView _tankView)
+    {
+        TankModel = _tankModel;
+        TankView = _tankView;
+
+        TankModel.SetTankController(this);
+        TankView.SetTankController(this);
+
+        GameObject.Instantiate(TankView.gameObject);
+    }
+}
---
+++ Assets/Scripts/tankModel.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class tankModel
+{
+    private tankController TankController;
+
+    public tankModel()
+    {
+
+    }
+
+    public void SetTankController(tankController _TankController)
+    {
+        TankController = _TankController;
+    }
+}
